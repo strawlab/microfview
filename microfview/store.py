a7dad3d00d09c7ed97525266aabe5274755929c4
John Stowers <john.stowers@gmail.com>
2015-09-10 18:14:23 +0200

    store: use real types
    using named tuples was a little faster but much
    more buggy as they
     * can be iterated over, which made it hard to tell if
       it was a list of them, or one of them
     * did weird things with __eq__ when they contained
       numpy arrays, e.g. an instance of ContourType could
       not be checked if it was in a dictionary because the
       namedtuple would delegate to the array instance

--- microfview/store.py
+++ microfview/store.py
@@ -4,11 +4,8 @@ DETECTED_OBJECT   = "UFVIEW_object"
 TRACKED_OBJECT    = "UFVIEW_tracked_object"
 CONTOUR           = "UFVIEW_contour"
 
-SPECIAL_STATE_KEYS = set((DETECTED_OBJECT, TRACKED_OBJECT, CONTOUR))
+SPECIAL_STATE_KEYS = {DETECTED_OBJECT, TRACKED_OBJECT, CONTOUR}
 
-DetectedObjectType = collections.namedtuple('TrackedObject', ['id', 'x', 'y'])
-TrackedObjectType = collections.namedtuple('TrackedObject', ['id', 'x', 'y', 'err'])
-ContourType = collections.namedtuple("Contour", ['id', 'x', 'y', 'pts'])
 
 def state_update(old, new, nick):
     for k in new:
@@ -22,6 +19,39 @@ def state_update(old, new, nick):
                 old[k].append(_new)
         else:
             old[k] = new[k]
+
+
+class DetectedObjectType:
+    def __init__(self, id, x, y):
+        self.id = int(id)
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "DetectedObject(id=%d, x=%s, y=%s)" % (self.id, self.x, self.y)
+
+
+class ContourType:
+    def __init__(self, id, x, y, pts):
+        self.id = int(id)
+        self.x = x
+        self.y = y
+        self.pts = pts
+
+    def __repr__(self):
+        return "Contour(id=%d, x=%s, y=%s, pts=...)" % (self.id, self.x, self.y)
+
+
+class TrackedObjectType:
+    def __init__(self, id, x, y, err):
+        self.id = int(id)
+        self.x = x
+        self.y = y
+        self.err = err
+
+    def __repr__(self):
+        return "TrackedObject(id=%d, x=%s, y=%s, err=%s)" % (self.id, self.x, self.y, self.err)
+
 
 class FrameStore(object):
 
