72597671825bbb5acc806ad657a9af0368b190ed
John Stowers <john.stowers@gmail.com>
2015-09-23 16:37:08 +0200

    store: add a PointArray type

--- microfview/store.py
+++ microfview/store.py
@@ -1,15 +1,14 @@
-import numpy as np
-import collections
-
 DETECTED_OBJECT   = "UFVIEW_object"
 TRACKED_OBJECT    = "UFVIEW_tracked_object"
 TRACKED_3D_OBJECT = "UFVIEW_tracked_3d_object"
 CONTOUR           = "UFVIEW_contour"
+POINT_ARRAY       = "UFVIEW_point_array"
 
-SPECIAL_STATE_KEYS = {DETECTED_OBJECT, TRACKED_OBJECT, TRACKED_3D_OBJECT, CONTOUR}
+SPECIAL_STATE_KEYS = {DETECTED_OBJECT, TRACKED_OBJECT, TRACKED_3D_OBJECT, CONTOUR, POINT_ARRAY}
 
 UNIT_METERS = "m"
 UNIT_PIXELS = "px"
+
 
 class DetectedObjectType:
     def __init__(self, id, x, y):
@@ -53,6 +52,15 @@ class Tracked3DObjectType:
 
     def __repr__(self):
         return "Tracked3DObject(id=%d, x=%s, y=%s, z=%s err=%s)" % (self.id, self.x, self.y, self.z, self.err)
+
+
+class PointArrayType:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "PointArray([[%s,%s]...[%s,%s]], %d pts)" % (self.x[0],self.y[0],self.x[-1],self.y[-1],len(self.x))
 
 
 class FrameStore(object):
