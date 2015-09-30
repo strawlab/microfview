058e22810ab9c517ae109e3dca4a5fc6c733b223
John Stowers <john.stowers@gmail.com>
2015-09-30 23:46:25 +0200

    plugins.transform: attact a transform matrix describing the ROI
    this can be used later in the frame store, and in the display
    plugin

--- microfview/plugins/transform.py
+++ microfview/plugins/transform.py
@@ -1,3 +1,4 @@
+import yaml
 import numpy as np
 
 from ..plugin import BlockingPlugin
@@ -12,11 +13,20 @@ class ExtractROIPlugin(BlockingPlugin):
         # roi is ((x0,y0), (x1,y1))
         self._roi_col, self._roi_row = slice(int(roi[0][0]),int(roi[1][0])), slice(int(roi[0][1]),int(roi[1][1]))
         self._copy_data = copy_data
+        # create a 2x3 affine transform matrix representing the translation
+        self._M = np.float32([[1,0,roi[0][0]],[0,1,roi[0][1]]])
+
+    @classmethod
+    def from_yaml(cls, path, **kwargs):
+        with open(path) as f:
+            dat = yaml.safe_load(f)
+            return cls(dat['transform']['background']['roi'][0], **kwargs)
 
     def process_frame(self, frame, frame_number, frame_count, frame_time, current_time, state):
         # slice works if image is 1 or 3 channel
         img = frame[self._roi_row, self._roi_col]
         if self._copy_data:
-            return img.copy()
+            _img = img.copy()
         else:
-            return img
+            _img = img
+        return _img, {'FRAME_TRANSFORM':self._M}
